bdc0a4402749eb135861aa88fc56ba6128c93eb5
tanxeem <itx.tanxeem@gmail.com>
2025-06-03 09:00:17 +0300

    Order Model

---
+++ backend/src/models/order.models.js
@@ -0,0 +1,85 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  orderItems: [
+    {
+      book: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Book',
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+        min: 1,
+      },
+      price: {
+        type: Number,
+        required: true,
+        min: 0,
+      },
+    },
+  ],
+  shippingAddress: {
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    postalCode: { type: String, required: true },
+    country: { type: String, required: true },
+  },
+  paymentMethod: {
+    type: String,
+    required: true,
+    enum: ['Credit Card', 'Debit Card', 'Easy Paisa', 'Jazz Cash', 'COD'],
+    default: 'COD',
+  },
+  paymentResult: {
+    id: String,
+    status: String,
+    update_time: String,
+    email_address: String,
+  },
+  itemsPrice: {
+    type: Number,
+    required: true,
+    default: 0.0,
+  },
+  taxPrice: {
+    type: Number,
+    required: true,
+    default: 0.0,
+  },
+  shippingPrice: {
+    type: Number,
+    required: true,
+    default: 0.0,
+  },
+  totalPrice: {
+    type: Number,
+    required: true,
+    default: 0.0,
+  },
+  isPaid: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  paidAt: {
+    type: Date,
+  },
+  isDelivered: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  deliveredAt: {
+    type: Date,
+  },
+}, { timestamps: true });
+
+const Order = mongoose.model('Order', orderSchema);
+export default Order;
